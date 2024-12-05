1e0791d20ca97749e045d8b95820ea6b2cadcbff
pooja <pooja06bhosale@gmail.com>
2024-12-06 00:58:29 +0530

    added e

--- src/main/java/com/example/pooja_spring_first_project/SayHello.java
+++ src/main/java/com/example/pooja_spring_first_project/SayHello.java
@@ -13,10 +13,10 @@ public class SayHello {
         
     }
     // here name is variable so we put into curly braces--hello is constant
-    @RequestMapping("/hello/{nam}")
+    @RequestMapping("/hello/{name}")
     // pathvariable used for-- from  map taking  name variable and map with String name
-    public String SayHellotosomeone(@PathVariable("nam") String nam){
-        return "Hello"+ " "+nam;
+    public String SayHellotosomeone(@PathVariable("name") String name){
+        return "Hello"+ " "+name;
 
     }
 }
